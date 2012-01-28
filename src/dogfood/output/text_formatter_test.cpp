7518cdfaafbde257abf3b967396611b85dd442ca
Bjorn Fahller <bjorn@fahller.se>
2012-01-28 14:13:17 +0100

    dogfood text formatter test of code conversion

--- src/dogfood/output/text_formatter_test.cpp
+++ src/dogfood/output/text_formatter_test.cpp
@@ -759,5 +759,143 @@ TESTSUITE(output)
         ASSERT_PRED(crpcut::regex(re, crpcut::regex::m), test_buffer.os.str());
       }
     }
+
+    TEST(replace_illegal_chars_when_output_is_ASCII, fix)
+    {
+      ASSERT_SCOPE_HEAP_LEAK_FREE
+      {
+        StrictMock<mock::stream_buffer> test_buffer;
+        crpcut::output::text_formatter obj(test_buffer,
+                                           "one",
+                                           1,
+                                           vec,
+                                           tags,
+                                           test_modifier,
+                                           "ASCII");
+        obj.begin_case(s("apa"), false, false);
+
+        char buff[255];
+        for (size_t i = 0; i < sizeof(buff); ++i)
+          {
+            buff[i] = char(i + 1);
+          }
+
+        obj.print(s("info"),
+                  crpcut::datatypes::fixed_string::make(buff, 255));
+        obj.terminate(crpcut::creating, s("katt"), s(""));
+        obj.end_case();
+        const char pattern[] =
+          "<NF>FAILED?: apa\n"
+          "info---------------------------------------------------------------------------\n"
+          "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
+          " !\"#$%&'()*+,-./0123456789:;<=>?"
+          "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+          "`abcdefghijklmnopqrstuvwxyz{|}~\x7f"
+          "................................................................"
+          "................................................................\n"
+          "-------------------------------------------------------------------------------\n"
+          "phase=\"creating\"  -------------------------------------------------------------\n"
+          "katt\n"
+          "-------------------------------------------------------------------------------\n"
+          "<>===============================================================================\n"
+          ;
+        std::string str = test_buffer.os.str();
+        std::ostringstream os;
+        std::ostringstream chars;
+        for (size_t i = 0; i < str.length(); ++i)
+          {
+            if ((i & 15) == 0) { os << std::setw(3) << std::dec << i; }
+            chars << (isprint(str[i]) ? str[i] : '.');
+            os << ' ' << std::hex << std::setw(2) << std::setfill('0')
+               << unsigned((unsigned char)str[i]);
+            if ((i & 15) == 15)
+              {
+                os << "  " << chars.str() << '\n';
+                chars.str(std::string());
+              }
+          }
+        INFO << os.str();
+        for (size_t i = 0; i < sizeof(pattern); ++i)
+          {
+            if (pattern[i] != str[i])
+              {
+                FAIL << "i=" << i << " str[i]=" << str[i] << " pattern[i]=" << pattern[i];
+              }
+          }
+      }
+    }
+
+    TEST(verbatim_copy_of_all_chars_when_output_is_not_defined, fix)
+    {
+      ASSERT_SCOPE_HEAP_LEAK_FREE
+      {
+        StrictMock<mock::stream_buffer> test_buffer;
+        crpcut::output::text_formatter obj(test_buffer,
+                                           "one",
+                                           1,
+                                           vec,
+                                           tags,
+                                           test_modifier,
+                                           no_char_conversion);
+        obj.begin_case(s("apa"), false, false);
+
+        char buff[255];
+        for (size_t i = 0; i < sizeof(buff); ++i)
+          {
+            buff[i] = char(i + 1);
+          }
+
+        obj.print(s("info"),
+                  crpcut::datatypes::fixed_string::make(buff, 255));
+        obj.terminate(crpcut::creating, s("katt"), s(""));
+        obj.end_case();
+        const char pattern[] =
+          "<NF>FAILED?: apa\n"
+          "info---------------------------------------------------------------------------\n"
+          "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
+          " !\"#$%&'()*+,-./0123456789:;<=>?"
+          "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+          "`abcdefghijklmnopqrstuvwxyz{|}~\x7f"
+          "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f"
+          "\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f"
+          "\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf"
+          "\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf"
+          "\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf"
+          "\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf"
+          "\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef"
+          "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff\n"
+          "-------------------------------------------------------------------------------\n"
+          "phase=\"creating\"  -------------------------------------------------------------\n"
+          "katt\n"
+          "-------------------------------------------------------------------------------\n"
+          "<>===============================================================================\n"
+          ;
+        std::string str = test_buffer.os.str();
+        std::ostringstream os;
+        std::ostringstream chars;
+        for (size_t i = 0; i < str.length(); ++i)
+          {
+            if ((i & 15) == 0) { os << std::setw(3) << std::dec << i; }
+            chars << (isprint(str[i]) ? str[i] : '.');
+            os << ' ' << std::hex << std::setw(2) << std::setfill('0')
+               << unsigned((unsigned char)str[i]);
+            if ((i & 15) == 15)
+              {
+                os << "  " << chars.str() << '\n';
+                chars.str(std::string());
+              }
+          }
+        INFO << os.str();
+        for (size_t i = 0; i < sizeof(pattern); ++i)
+          {
+            if (pattern[i] != str[i])
+              {
+                FAIL << "i=" << i << " str[i]=" << str[i] << " pattern[i]=" << pattern[i];
+              }
+          }
+      }
+    }
   }
 }
